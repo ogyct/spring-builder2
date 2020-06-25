04f94f9c00ae0c3013157c910c4a84144c0c465e
Dmitry Avgustis <dmitry.avgustis@zentity.com>
2020-06-25 11:44:19 +0200

    project init

---
+++ src/main/java/com/example/corona2/CoronaDisinfectant.java
@@ -0,0 +1,16 @@
+package com.example.corona2;
+
+public class CoronaDisinfectant {
+    public void start () {
+        // todo alert everyone about disinfection and ask them to go away
+        // todo additionally ask everyone who didn't listen for the first time
+
+        disinfect();
+        // todo  alert everyone they can go back
+
+    }
+
+    private void disinfect() {
+        System.out.println("Prayer is being read: CORONA GO AWAY, AMEN!");
+    }
+}
---
+++ src/main/java/com/example/corona2/Main.java
@@ -0,0 +1,10 @@
+package com.example.corona2;
+
+public class Main {
+
+    public static void main(String[] args) {
+        CoronaDisinfectant coronaDisinfectant = new CoronaDisinfectant();
+        coronaDisinfectant.start();
+    }
+
+}
